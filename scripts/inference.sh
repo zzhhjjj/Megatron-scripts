c4c94189f356a6f7f113540a415b6aa718b6c76f
haojun.zhao@huggingface.co <haojun_zhao@ip-26-0-160-103.ec2.internal>
2025-04-17 13:10:47 +0000

    toadd

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,4 +1,5 @@
-
+# follow the instructions in README.md
+# this doesn't work
 script_path=/fsx/haojun/Megatron-LM/examples/inference/llama_mistral/run_text_generation_llama3.sh
 path_to_converted_core_checkpoint=/fsx/haojun/Megatron-files/megatron_checkpoints/Meta-Llama-3-8B/iter_0000001
 path_to_downloaded_huggingface_checkpoint=/fsx/haojun/Megatron-files/hf_checkpoints/Meta-Llama-3-8B
